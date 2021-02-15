c9466ea43f7a7deb68931fbba6f55f7754696d77
Jakub Kupczyk <69753899+jkbkupczyk@users.noreply.github.com>
2021-02-15 10:28:57 +0100

    Update 11_arr_sorting.php

--- PHP/lectures/11_arrays/11_arr_sorting.php
+++ PHP/lectures/11_arrays/11_arr_sorting.php
@@ -1,9 +1,66 @@
 <?php
 
-$fruits = array("cytryna", "pomarancz", "banan", "melon");
+$tab = array(20, 1, 5, 75, 5, -120, 100);
 
-sort($fruits);
-
-foreach ($fruits as $key => $val) {
-    echo "fruits[" . $key . "] = " . $val . "\n";
+function t($tab)
+{
+    foreach($tab as $val){
+        echo "$val ";
+    }
+    echo "<br>";
 }
+
+t($tab);
+
+sort($tab);
+t($tab);
+
+rsort($tab);
+t($tab);
+
+$names = array('Katarzyna', 'anna', 'zbigniew', 'Mateusz', 'TEST');
+t($names);
+
+sort($names);
+t($names);
+
+function replaceToLowerCase(&$tab)
+{
+    foreach($tab as $key => $val){
+        $replace = strtolower($val);
+        $tab[$key] = $replace;
+        echo "$replace ";
+    }
+}
+
+sort($names);
+t($names);
+
+replaceToLowerCase($names);
+echo "<br>";
+sort($names);
+t($names);
+
+$tabAssoc = array(
+    'name' => 'Jakub',
+    'surname' => 'Kupczyk',
+    'age' => 20,
+    'nick' => 'KuKaa'
+);
+
+t($tabAssoc);
+
+asort($tabAssoc);
+t($tabAssoc);
+
+arsort($tabAssoc);
+t($tabAssoc);
+
+ksort($tabAssoc);
+t($tabAssoc);
+
+krsort($tabAssoc);
+t($tabAssoc);
+
+
+
